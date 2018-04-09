58e669d5879e46e54720e728ceba8774cbd2dce2
Jérémy GARROUSTE <jeremy.garrouste@gmail.com>
2018-04-09 16:55:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 """Simple pylint Demo"""
-from flask import Flask
+from flask import Flask, jsonify
 APP = Flask(__name__)
 
-@APP.route("/")
-def hello():
-    """return message"""
-    return "Hello World!"
+MESSAGES = [
+    {
+        'id': 1,
+        'message': u'Paris',
+    },
+    {
+        'id': 2,
+        'message': u'Container Training',
+    },
+]
+
+@APP.route('/', methods=['GET'])
+def messages():
+    """Return JSON object"""
+    return jsonify({'messages': MESSAGES})
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0')
